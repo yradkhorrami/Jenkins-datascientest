096456183571b0a2637db57e4c7831c0d80827d3
yradkhorrami <yaser.radkhorrami@desy.de>
2025-09-16 13:22:45 +0200

    we did it!

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 app = FastAPI()
+
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest !!"}
+    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD Pipeline!!"}
